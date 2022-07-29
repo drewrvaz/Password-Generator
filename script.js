d0230adf89b1634c2b4b6494575144f01f5869f7
drewrvaz <drewrvaz@gmail.com>
2022-07-29 12:24:30 -0400

    Reset the JavaScript doc to the beginning. I'm confusing myself right now

--- script.js
+++ script.js
@@ -3,14 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 var writePassword = function() {
-  
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var passwordlength = window.prompt("How many characters do you want in is this password?");
-  if (passwordlength < 8) {
-    window.alert("Please enter a number greater than 7")
-  } else if (passwordlength > 128) {
-    window.alert ("Please enter a number less then 129")
-  } else 
   
   passwordText.value = password;
 
