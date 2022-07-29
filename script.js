edd5ce11d38af46c206c0bf059aae8af7a834bcc
drewrvaz <drewrvaz@gmail.com>
2022-07-29 17:45:26 -0400

    Reset the doc to the beginning again. I want to do this correctly, and I don't like the direction I'm going

--- script.js
+++ script.js
@@ -3,19 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var passwordLength = prompt("How many characters do you want your password to be? It must be at least 8 characters, and no more than 128.");
-  var lowerCase = prompt("Do you want lowercase letters included in your password? yes or no.");
-  var upperCase = prompt("Do you want uppercase letters in your password? yes or no.");
-  var numbers = prompt("Do you want numbers in your password? yes or no.");
-  var symbols = prompt("Do you want symbols in your password? yes or no.");
+  var password = generatePassword() 
+  var passwordText = document.querySelector("#password");
 
-console.log(passwordLength);
-console.log(lowerCase);
-console.log(upperCase);
-console.log(numbers);
-console.log(symbols);
+  passwordText.value = password;
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
