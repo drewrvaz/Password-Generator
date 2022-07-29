cf2e1b031dfc855b647bf871062705a37ba00b4d
drewrvaz <drewrvaz@gmail.com>
2022-07-29 18:34:47 -0400

    inserted code to make button functional, currently not follwoing any criteria

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword() 
+  var password = generatePassword(); 
+      
+  function generatePassword() {
+    var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+  }
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,3 +22,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
