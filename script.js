8ef1fcd8ff540933a66d6077bb7f2c4347e86af2
drewrvaz <drewrvaz@gmail.com>
2022-08-02 09:05:41 -0400

    Created arrays for all the character requirements in the password

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ var passSymbol
 var passLower
 var passUpper
 
-// Write password to the #password input
+//Creating arrays for each of the variables required in the password
+number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//Only added the special characters that actually worked within the array
+symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "`", "~", ":", ";", ",", "<", ">", "/", "?", "[", "]", "{", "}", "|"]
+//Write password to the #password input
 function writePassword() {
   
   function passwordPrompt() {
