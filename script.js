3dbaac715cef2e879538d25408969fb57f0b009f
drewrvaz <drewrvaz@gmail.com>
2022-08-03 16:28:51 -0400

    Tried to make a change to see if I could make it run. It did not work, then reset the code to make sure that it was functioning like before

--- script.js
+++ script.js
@@ -22,81 +22,82 @@ symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", 
 
 // Write password to the #password input
 function writePassword() {
-  
-  var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
+  var password = generatePassword();
 
-  // Function for using the variables to generate the password
-  function generatePassword() {
-    // Prompts the user to enter a character amount
-    passLength = parseInt(prompt("How many characters would you like your password to be? It must be between 8 and 128 characters."));
-    // If the user fails to enter a password length, this notify them that they need to answer the first prompt  
-    if (!passLength) {
-        alert("You need to enter a length for your password"); 
-      }
-      // If the user fails to enter a number between 8 and 128, this will alert them that they need to choose a different number 
-      else if (passLength < 8 || passLength > 128)
-      passLength = parseInt(prompt("This needs to be between 8 and 128 characters"));
-      // Tells user to begin filling out the remaining requirements for the password once meeting the length requirements. This utilizes confirm, which will allow bollean to be used to define the password requirements in the generator
-      else { 
-        passNumber = confirm("Do you want this password include numbers? Ok for yes, cancel for no.");
-        passSymbol = confirm("Do you want this password to include special characters? Ok for yes, cancel for no.");
-        passLower = confirm("Do you want this password to include lowercase letters? Ok for yes, cancel for no.");
-        passUpper = confirm("Do you want this password to include uppercase letters? Ok for yes, cancel for no.");
-      };
-
-      // Created an alert if the user doesn't choose any criteria for the password
-      if (!passNumber && !passSymbol && !passLower && !passUpper) {
-        criteria = alert("You have to pick something to make your password");
-      } 
-      // Used concat to combine all the arrays if the user wants all options in the password
-      else if (passNumber && passSymbol && passLower && passUpper) {
-        criteria = number.concat(symbol, upperCase, lowerCase);
-      }
-      // Created else if statements for each of the possibilities if the user wants three of the four options, and used concat to combine each combination of the arrays
-      else if (passNumber && passSymbol && passLower && !passUpper) {
-        criteria = number.concat(symbol, lowerCase);
-      } else if (passNumber && passSymbol && !passLower && passUpper) {
-        criteria = number.concat(symbol, upperCase);
-      } else if (passNumber && !passSymbol && passLower && passUpper) {
-        criteria = number.concat(lowerCase, upperCase);
-      } else if (!passNumber && passSymbol && passLower && passUpper) {
-        criteria = symbol.concat(lowerCase, upperCase);
-      }
-      // Created else if statements for each of the possibilities if the user wants two of the four options, and used concat to combine each combination of the arrays
-       else if (passNumber && !passSymbol && !passLower && passUpper) {
-        criteria = number.concat(upperCase);
-      } else if (passNumber && passSymbol && !passLower && !passUpper) {
-        criteria = number.concat(symbol);
-      } else if (passNumber && !passSymbol && passLower && !passUpper) {
-        criteria = number.concat(lowerCase);
-      } else if (!passNumber && passSymbol && !passLower && passUpper) {
-        criteria = symbol.concat(upperCase);
-      } else if (!passNumber && passSymbol && passLower && !passUpper) {
-        criteria = symbol.concat(lowerCase);
-      } else if (!passNumber && !passSymbol && passLower && passUpper) {
-        criteria = lowerCase.concat(upperCase);
-      }
-      // Created else if statements for each of the possibilities if the user wants one of the four options, and tagged it to the correct array
-      else if (passNumber && !passSymbol && !passLower && !passUpper) {
-        criteria = number;
-      } else if (!passNumber && passSymbol && !passLower && !passUpper) {
-        criteria = symbol;
-      } else if (!passNumber && !passSymbol && passLower && !passUpper) {
-        criteria = lowerCase;
-      } else if (!passNumber && !passSymbol && !passLower && passUpper) {
-        criteria = upperCase;
-      }
-
-      //For loop to choose random random characters for the length requested by the user
-      for (let i = 0; i < passLength, i++;) {
-        var chooseCriteria = criteria[Math.floor(Math.random() * criteria.length)];
-        password.push(chooseCriteria);
-      }
+// Function for using the variables to generate the password
+function generatePassword() {
+  // Prompts the user to enter a character amount
+  passLength = parseInt(prompt("How many characters would you like your password to be? It must be between 8 and 128 characters."));
+  // If the user fails to enter a password length, this notify them that they need to answer the first prompt  
+  if (!passLength) {
+    alert("You need to enter a length for your password"); 
+  }
+  // If the user fails to enter a number between 8 and 128, this will alert them that they need to choose a different number 
+  else if (passLength < 8 || passLength > 128) {
+    passLength = parseInt(prompt("This needs to be between 8 and 128 characters"));
   }
 
+  // Tells user to begin filling out the remaining requirements for the password once meeting the length requirements. This utilizes confirm, which will allow bollean to be used to define the password requirements in the generator
+  else { 
+    passNumber = confirm("Do you want this password include numbers? Ok for yes, cancel for no.");
+    passSymbol = confirm("Do you want this password to include special characters? Ok for yes, cancel for no.");
+    passLower = confirm("Do you want this password to include lowercase letters? Ok for yes, cancel for no.");
+    passUpper = confirm("Do you want this password to include uppercase letters? Ok for yes, cancel for no.");
+    };
+
+  // Created an alert if the user doesn't choose any criteria for the password
+    if (!passNumber && !passSymbol && !passLower && !passUpper) {
+      criteria = alert("You have to pick something to make your password");
+    } 
+    // Used concat to combine all the arrays if the user wants all options in the password
+    else if (passNumber && passSymbol && passLower && passUpper) {
+      criteria = number.concat(symbol, upperCase, lowerCase);
+    }
+  // Created else if statements for each of the possibilities if the user wants three of the four options, and used concat to combine each combination of the arrays
+    else if (passNumber && passSymbol && passLower && !passUpper) {
+      criteria = number.concat(symbol, lowerCase);
+    } else if (passNumber && passSymbol && !passLower && passUpper) {
+      criteria = number.concat(symbol, upperCase);
+    } else if (passNumber && !passSymbol && passLower && passUpper) {
+      criteria = number.concat(lowerCase, upperCase);
+    } else if (!passNumber && passSymbol && passLower && passUpper) {
+      criteria = symbol.concat(lowerCase, upperCase);
+    }
+    // Created else if statements for each of the possibilities if the user wants two of the four options, and used concat to combine each combination of the arrays
+    else if (passNumber && !passSymbol && !passLower && passUpper) {
+      criteria = number.concat(upperCase);
+    } else if (passNumber && passSymbol && !passLower && !passUpper) {
+      criteria = number.concat(symbol);
+    } else if (passNumber && !passSymbol && passLower && !passUpper) {
+      criteria = number.concat(lowerCase);
+    } else if (!passNumber && passSymbol && !passLower && passUpper) {
+      criteria = symbol.concat(upperCase);
+    } else if (!passNumber && passSymbol && passLower && !passUpper) {
+      criteria = symbol.concat(lowerCase);
+    } else if (!passNumber && !passSymbol && passLower && passUpper) {
+      criteria = lowerCase.concat(upperCase);
+    }
+    // Created else if statements for each of the possibilities if the user wants one of the four options, and tagged it to the correct array
+    else if (passNumber && !passSymbol && !passLower && !passUpper) {
+      criteria = number;
+    } else if (!passNumber && passSymbol && !passLower && !passUpper) {
+      criteria = symbol;
+    } else if (!passNumber && !passSymbol && passLower && !passUpper) {
+      criteria = lowerCase;
+    } else if (!passNumber && !passSymbol && !passLower && passUpper) {
+      criteria = upperCase;
+    }
+
+    //For loop to choose random characters for the length requested by the user
+    for (let i = 0; i < passLength, i++;) {
+      var chooseCriteria = criteria[Math.floor(Math.random() * criteria.length)];
+      password.push(chooseCriteria);
+    }
   passwordText.value = password;
+  }
 }
+  
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
